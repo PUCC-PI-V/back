ae8ae1d00150bf9deb1e59e29a55870b97764d52
Gabriel S. Freire <146298797+GabrielFreire05@users.noreply.github.com>
2026-05-30 22:26:15 -0300

    feat: Implementação do back com o front

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ PORT = int(os.getenv("PORT", 8000))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080"],
+    allow_origins=["http://localhost", "http://localhost:8080", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
